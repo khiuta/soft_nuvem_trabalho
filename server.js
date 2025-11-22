62c6847256bb5ebf1a2f3cf04db1e46b53d542f9
khiuta <minhaepic1551@gmail.com>
2025-11-22 00:21:35 -0300

    fix type in commonjs files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import db from './models/index.js'
 
 const { sequelize } = db;
 
-const port = 5000;
+const port = 80;
 
 app.listen(port, '0.0.0.0', async () => {
     await sequelize.authenticate();
