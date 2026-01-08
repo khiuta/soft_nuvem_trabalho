536f7ed0f239aae2bbd3a619c8dcb0e66daef0a4
khiuta <minhaepic1551@gmail.com>
2026-01-07 21:36:16 -0300

    implementing rabbitmq and minio

--- controllers/management/loanController.js
+++ controllers/management/loanController.js
@@ -1,21 +1,27 @@
-import db from '../../models/index.js'
+import db from "../../models/index.js";
 
-import { DateTime, Settings } from 'luxon';
+import { DateTime, Settings } from "luxon";
 
-import { dynamoClient } from '../../lib/dynamoClient.js';
-import { PutItemCommand } from '@aws-sdk/client-dynamodb';
-import { marshall } from '@aws-sdk/util-dynamodb';
-import dynamoLogs from '../../lib/dynamoLogs.js';
+import { dynamoClient } from "../../lib/dynamoClient.js";
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import dynamoLogs from "../../lib/dynamoLogs.js";
 
-const { Loan, Book } = db;
+const { Loan, Book, Student } = db;
 
 class LoanController {
   // essa função é chamada depois de verificar se o aluno já existe no banco de dados
-  async store(req, res){
-    const { book_name, book_author, book_publisher, student_matricula, student_name } = req.body;
+  async store(req, res) {
+    const {
+      book_name,
+      book_author,
+      book_publisher,
+      student_matricula,
+      student_name,
+    } = req.body;
 
     try {
-      Settings.defaultLocale = 'pt-BR';
+      Settings.defaultLocale = "pt-BR";
       const loan_date = DateTime.now();
       const return_date = loan_date.plus({ days: 30 });
 
@@ -23,20 +29,38 @@ class LoanController {
         where: {
           title: book_name,
           author: book_author,
-          publisher: book_publisher
-        }
-      })
+          publisher: book_publisher,
+        },
+      });
 
       const book_id = book.id;
-      const newLoan = await Loan.create({ loan_date, return_date, book_id, student_matricula, returned: false, pendent: false, book_name, student_name });
+      const newLoan = await Loan.create({
+        loan_date,
+        return_date,
+        book_id,
+        student_matricula,
+        returned: false,
+        pendent: false,
+        book_name,
+        student_name,
+      });
 
-      const logData = { book_id, student_matricula, loan_date: loan_date.toISO(), return_date: return_date.toISO(), returned: false, pendent: false, book_name, student_name }
+      const logData = {
+        book_id,
+        student_matricula,
+        loan_date: loan_date.toISO(),
+        return_date: return_date.toISO(),
+        returned: false,
+        pendent: false,
+        book_name,
+        student_name,
+      };
 
       const item = dynamoLogs.create_loan(logData, true);
 
       const command = new PutItemCommand({
         TableName: "api-logs",
-        Item: marshall(item, { removeUndefinedValues: true })
+        Item: marshall(item, { removeUndefinedValues: true }),
       });
 
       await dynamoClient.send(command);
@@ -46,22 +70,22 @@ class LoanController {
     } catch (error) {
       console.error(error);
       return res.status(500).json({
-        error: 'Ocorreu um erro no servidor',
-        details: error.message
+        error: "Ocorreu um erro no servidor",
+        details: error.message,
       });
     }
   }
 
-  async index(req, res){
+  async index(req, res) {
     try {
       const loans = await Loan.findAll();
 
-      if(loans.length > 0){
+      if (loans.length > 0) {
         const item = dynamoLogs.get_all_loans(loans.length, true);
 
         const command = new PutItemCommand({
           TableName: "api-logs",
-          Item: marshall(item, { removeUndefinedValues: true })
+          Item: marshall(item, { removeUndefinedValues: true }),
         });
 
         await dynamoClient.send(command);
@@ -73,7 +97,7 @@ class LoanController {
 
         const command = new PutItemCommand({
           TableName: "api-logs",
-          Item: marshall(item, { removeUndefinedValues: true })
+          Item: marshall(item, { removeUndefinedValues: true }),
         });
 
         await dynamoClient.send(command);
@@ -84,28 +108,32 @@ class LoanController {
     } catch (error) {
       console.error(error);
       return res.status(500).json({
-        error: 'Ocorreu um erro no servidor.',
-        details: error.message
+        error: "Ocorreu um erro no servidor.",
+        details: error.message,
       });
     }
   }
 
-  async update(req, res){
+  async update(req, res) {
     const { id, loan_date, return_date, book_id, student_matricula } = req.body;
 
     try {
       const loanToUpdate = await Loan.findOne({
         where: {
           id,
-        }
+        },
       });
 
-      if(loanToUpdate) {
+      if (loanToUpdate) {
         // verifica se o dado foi enviado, se sim, atualiza, se não, continua o dado antigo
         loanToUpdate.loan_date = loan_date ? loan_date : loanToUpdate.loan_date;
-        loanToUpdate.return_date = return_date ? return_date : loanToUpdate.return_date;
+        loanToUpdate.return_date = return_date
+          ? return_date
+          : loanToUpdate.return_date;
         loanToUpdate.book_id = book_id ? book_id : loanToUpdate.book_id;
-        loanToUpdate.student_matricula = student_matricula ? student_matricula : loanToUpdate.student_matricula;
+        loanToUpdate.student_matricula = student_matricula
+          ? student_matricula
+          : loanToUpdate.student_matricula;
 
         const updatedLoan = await loanToUpdate.save();
 
@@ -113,7 +141,7 @@ class LoanController {
 
         const command = new PutItemCommand({
           TableName: "api-logs",
-          Item: marshall(item, { removeUndefinedValues: true })
+          Item: marshall(item, { removeUndefinedValues: true }),
         });
 
         await dynamoClient.send(command);
@@ -125,44 +153,47 @@ class LoanController {
 
         const command = new PutItemCommand({
           TableName: "api-logs",
-          Item: marshall(item, { removeUndefinedValues: true })
+          Item: marshall(item, { removeUndefinedValues: true }),
         });
 
         await dynamoClient.send(command);
         console.log("Log saved to DynamoDB.");
 
-        return res.status(404).json({ message: 'Esse empréstimo não existe.' });
+        return res.status(404).json({ message: "Esse empréstimo não existe." });
       }
     } catch (error) {
       console.error(error);
       return res.status(500).json({
-        error: 'Ocorreu um erro no servidor.',
-        details: error.message
+        error: "Ocorreu um erro no servidor.",
+        details: error.message,
       });
     }
   }
 
-  async devolution(req, res){
+  async devolution(req, res) {
     const { id } = req.params;
 
     try {
       const loanToReturn = await Loan.findOne({
         where: {
           id,
-        }
+        },
       });
 
-      if(loanToReturn.returned == false) loanToReturn.returned = true;
-      else return res.status(400).json({ message: 'Esse empréstimo já foi retornado.' });
+      if (loanToReturn.returned == false) loanToReturn.returned = true;
+      else
+        return res
+          .status(400)
+          .json({ message: "Esse empréstimo já foi retornado." });
 
       const loanUpdated = await loanToReturn.save();
 
       return res.status(200).json({ loanUpdated });
-    } catch (error){
+    } catch (error) {
       console.error(error);
       return res.status(500).json({
-        error: 'Ocorreu um erro no servidor.',
-        details: error.message
+        error: "Ocorreu um erro no servidor.",
+        details: error.message,
       });
     }
   }
--- controllers/student/studentController.js
+++ controllers/student/studentController.js
@@ -1,22 +1,26 @@
-import db from '../../models/index.js'
+import db from "../../models/index.js";
 
 const { Student } = db;
 
-class StudentController{
-  async store(req, res){
+class StudentController {
+  async store(req, res) {
     const { matricula, full_name, course, period, can_borrow } = req.body;
 
     try {
       const newStudent = await Student.create({
-        matricula, full_name, course, period, can_borrow
+        matricula,
+        full_name,
+        course,
+        period,
+        can_borrow,
       });
 
       return res.status(200).json(newStudent);
-    } catch(error) {
-      console.error(error);
+    } catch (error) {
+      console.error(full_name);
       return res.status(500).json({
-        error: 'Ocorreu um erro no servidor.',
-        details: error.message
+        error: "Ocorreu um erro no servidor.",
+        details: error.message,
       });
     }
   }
@@ -25,7 +29,7 @@ class StudentController{
     try {
       const students = await Student.findAll();
 
-      if(students.length > 0){
+      if (students.length > 0) {
         return res.status(200).json(students);
       } else {
         return res.status(404).json({ message: "Nenhum aluno cadastrado." });
@@ -34,7 +38,7 @@ class StudentController{
       console.error(error);
       return res.status(500).json({
         error: "Ocorreu um erro no servidor.",
-        details: error.message
+        details: error.message,
       });
     }
   }
@@ -46,19 +50,21 @@ class StudentController{
       const student = await Student.findOne({
         where: {
           matricula,
-        }
+        },
       });
 
-      if(student){
+      if (student) {
         return res.status(200).json(student);
       } else {
-        return res.status(404).json({ message: 'Aluno não existe no banco de dados. '});
+        return res
+          .status(404)
+          .json({ message: "Aluno não existe no banco de dados. " });
       }
     } catch (error) {
       console.error(error);
       return res.status(500).json({
-        error: 'Ocorreu um erro no servidor.',
-        details: error.message
+        error: "Ocorreu um erro no servidor.",
+        details: error.message,
       });
     }
   }
@@ -68,29 +74,35 @@ class StudentController{
 
     try {
       const studentToUpdate = await Student.findOne({
-        where:{
+        where: {
           id,
-        }
+        },
       });
 
-      if(studentToUpdate){
-        studentToUpdate.matricula = matricula ? matricula : studentToUpdate.matricula;
-        studentToUpdate.full_name = full_name ? full_name : studentToUpdate.full_name;
+      if (studentToUpdate) {
+        studentToUpdate.matricula = matricula
+          ? matricula
+          : studentToUpdate.matricula;
+        studentToUpdate.full_name = full_name
+          ? full_name
+          : studentToUpdate.full_name;
         studentToUpdate.course = course ? course : studentToUpdate.course;
         studentToUpdate.period = period ? period : studentToUpdate.period;
-        studentToUpdate.can_borrow = can_borrow ? can_borrow : studentToUpdate.can_borrow;
+        studentToUpdate.can_borrow = can_borrow
+          ? can_borrow
+          : studentToUpdate.can_borrow;
 
         const updatedStudent = await studentToUpdate.save();
 
         return res.status(200).json(updatedStudent);
       } else {
-        return res.status(404).json({ message: 'Aluno não encontrado.' });
+        return res.status(404).json({ message: "Aluno não encontrado." });
       }
-    } catch(error) {
+    } catch (error) {
       console.error(error);
       return res.status(500).json({
-        error: 'Ocorreu um erro no servidor.',
-        details: error.message
+        error: "Ocorreu um erro no servidor.",
+        details: error.message,
       });
     }
   }
@@ -102,15 +114,15 @@ class StudentController{
       const studentToRemove = await Student.destroy({
         where: {
           id,
-        }
+        },
       });
 
       return res.status(200).json({ studentToRemove });
     } catch (error) {
       console.error(error);
       return res.status(500).json({
-        error: 'Ocorreu um erro no servidor.',
-        details: error.message
+        error: "Ocorreu um erro no servidor.",
+        details: error.message,
       });
     }
   }
